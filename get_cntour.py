56c95dfc0c534682c9f8afe5baac8bdddd5b95f6
netchenlaoshi <67730390@qq.com>
2020-03-20 13:40:53 +0800

    finish get cntour

--- get_cntour.py
+++ get_cntour.py
@@ -4,7 +4,7 @@ from  bs4 import  BeautifulSoup
 url="http://www.cntour.cn/"
 
 response=requests.get(url)
-# print(response.text)
+#print(response.text)
 
 soup=BeautifulSoup(response.text,'lxml')
 data=soup.select("#main > div > div.mtop.firstMod.clearfix > div.leftBox > "
