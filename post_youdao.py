eff20af3447124eb14a0b37253cec59793d9785b
netchenlaoshi <67730390@qq.com>
2020-03-31 10:07:10 +0800

    作业3 00-chenlaoshi finish time stamp

--- post_youdao.py
+++ post_youdao.py
@@ -2,15 +2,33 @@ import requests
 
 
 url="http://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule"
+
+
+def get_salt():
+    return '15846848803956'
+
+
+def get_sign():
+    return 'b1537e6e7d4296b0145432358da1fce0'
+
+
+def get_ts():
+    import time
+    t = time.time()
+    ts = str(int(round(t * 1000)))
+    return ts
+    # '1585615400595'
+
+
 form_data={
     'i':'我和你都是中国',
     'from':'AUTO',
     'to':'AUTO',
     'smartresult':'dict',
     'client':'fanyideskweb',
-    'salt':'15846848803956',
-    'sign':'b1537e6e7d4296b0145432358da1fce0',
-    'ts':'1584684880395',
+    'salt': get_salt(),
+    'sign': get_sign(),
+    'ts': get_ts(),
     'bv':'9ef61dc3d2f65f61d71a16bd47c6e9ee',
     'doctype':'json',
     'version':'2.1',
