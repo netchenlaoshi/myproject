757fa5e4ba5597b0ed76f2badccd4a16bff9a41a
chenhui <xiaotian712572@163.com>
2020-04-28 09:52:14 +0800

    finish today weather

--- chapter3/city_weather.py
+++ chapter3/city_weather.py
@@ -4,12 +4,21 @@ import requests
 class HeFeng():
     def __init__(self):
         self.url = "https://cdn.heweather.com/china-city-list.txt"
+        self.encoding='utf8'
+        self.pre_request="https://free-api.heweather.net/s6/weather/now?location="
+        # self.pre_request="https://free-api.heweather.net/v5/forecast?city="
+        self.sub_request="&key=30bfb68c37bb4ca78601591050e65ef0"
+
+    def today_weather(self,city_code):
+        dict=self.get_weather(city_code)
+        print(dict["HeWeather6"][0]['now'])
 
     def get_weather(self,city_code):
-        url="https://free-api.heweather.net/s6/weather/now?location="+city_code+"&key=30bfb68c37bb4ca78601591050e65ef0"
+        url=self.pre_request+city_code+self.sub_request
         info=requests.get(url)
-        info.encoding='utf8'
-        print(info.text)
+        info.encoding= self.encoding
+        return info.json()
+        # print(info.text)
 
     def get_city_code(self):
         cities = self.get_citys()
@@ -18,7 +27,7 @@ class HeFeng():
 
     def get_citys(self):
         html = requests.get(self.url)
-        html.encoding = 'utf8'
+        html.encoding = self.encoding
         cities = html.text.split('\n')
         return cities[6:]
 
@@ -26,5 +35,8 @@ class HeFeng():
 if __name__ == '__main__':
     hefeng = HeFeng()
     codes=hefeng.get_city_code()
-    hefeng.get_weather(codes.__next__())
+    for i in range(10):
+        # dict=hefeng.get_weather(codes.__next__())
+        # print(dict["HeWeather6"][0]['now']['tmp'])
+        hefeng.today_weather(codes.__next__())
 
