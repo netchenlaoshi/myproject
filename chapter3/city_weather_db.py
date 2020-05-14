d54e4403b6ac928943152b817c5abf37017dd9fb
netchenlaoshi <67730390@qq.com>
2020-05-14 22:31:51 +0800

    break time in class

---
+++ chapter3/city_weather_db.py
@@ -0,0 +1,20 @@
+import pymongo
+
+class HefengDb():
+    def __init__(self):
+        self.client = pymongo.MongoClient('localhost', 27017)
+        self.book_weather = self.client['weather']
+        self.sheet_weather = self.book_weather['sheet_weather_3']
+
+    def save(self,data):
+        self.sheet_weather.insert_one(data)
+
+    def show_all(self):
+        all=self.sheet_weather.find()
+        for each in all:
+            print(each)
+
+if __name__=="__main__":
+    hefengDb=HefengDb()
+    hefengDb.save({"name":"chenlaoshi","class":"net19049"})
+    hefengDb.show_all()
